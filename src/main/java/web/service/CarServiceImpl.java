3c3b5a302e8cb480f781e4a5d9c2eb7210d99c76
KonstantinMuravyev <k.muravyev2016@yandex.ru>
2023-09-07 01:39:20 +1000

    add files

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,23 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarServiceImpl implements CarService {
+
+    @Override
+    public List<Car> carList(Integer count) {
+        List<Car> carList;{
+            carList = new ArrayList<>();
+            carList.add(new Car("Toyota", "Window", 2003));
+            carList.add(new Car("Honda", "Prelude", 2011));
+            carList.add(new Car("Nissan", "President", 2008));
+            carList.add(new Car("Suzuki", "Escudo", 2017));
+            carList.add(new Car("Subaru", "LegacyB4", 2010));
+        }
+        return carList.stream().limit(count).collect(Collectors.toList());
+    }
+}
