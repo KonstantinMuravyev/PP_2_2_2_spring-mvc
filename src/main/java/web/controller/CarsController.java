dd24f0480c3665b817576fd340a578ce85c242ee
KonstantinMuravyev <k.muravyev2016@yandex.ru>
2023-09-09 16:37:01 +1000

    fix remarks

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -5,17 +5,23 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 import java.util.List;
 
 @Controller
 public class CarsController {
+
+    private final CarService carService;
+
+    public CarsController(CarService carService) {
+        this.carService = new CarServiceImpl();
+    }
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(defaultValue = "5") Integer count,
                             ModelMap model) {
-        CarServiceImpl carServiceImpl = new CarServiceImpl();
-        List<Car> cars = carServiceImpl.carList(count);
+        List<Car> cars = carService.carList(count);
         model.addAttribute("cars", cars);
         return "cars";
     }
